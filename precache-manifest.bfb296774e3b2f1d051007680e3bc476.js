d7da16380d8148069c635f21ef58109fbd3e125a
Ahlmen26 <colina.nielahlmen@gmail.com>
2020-04-07 23:49:24 +0800

    Add files via upload

---
+++ precache-manifest.bfb296774e3b2f1d051007680e3bc476.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "img/default-avatar.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "img/bg5.jpg"
+  },
+  {
+    "revision": "dcf3bf68e20425028353",
+    "url": "js/chunk-vendors.ca41697c.js"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "img/mike.jpg"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "img/img_3115.jpg"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "img/emilyz.jpg"
+  },
+  {
+    "revision": "9f729bae7214c1059ac15c5d6e44ffe6",
+    "url": "index.html"
+  },
+  {
+    "revision": "f04241ef410f0090c97f9829d4f6fba7",
+    "url": "widget.png"
+  },
+  {
+    "revision": "1f1254f82f6798f6b1f3a3054dbc841b",
+    "url": "widget_2.png"
+  },
+  {
+    "revision": "1020e00a302b13722d6e08e2b4bb2e83",
+    "url": "spaceship.png"
+  },
+  {
+    "revision": "ef3e78a78cb18cdf1b6255e3263bc798",
+    "url": "settings-gear-63.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "de7ca968cf11a0155b2c",
+    "url": "js/app.3ca0e898.js"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "img/favicon.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "img/apple-icon.png"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "img/anime3.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "de7ca968cf11a0155b2c",
+    "url": "css/app.8198cc5f.css"
+  },
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "apple-icon.png"
+  },
+  {
+    "revision": "68b3fbf265ee9125f767db9616999c92",
+    "url": "Data.json"
+  }
+];
